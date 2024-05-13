0366688033d2592eab18f711101852ab4709288f
cusmaria <163232769+cusmaria@users.noreply.github.com>
2024-05-13 14:25:45 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,45 +1,33 @@
-// glowAnimeにglowというクラス名を付ける定義
-function GlowAnimeControl() {
-    $('.glowAnime').each(function () {
-      var elemPos = $(this).offset().top - 50;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll >= elemPos - windowHeight) {
-        $(this).addClass("glow");
-  
-      } else {
-        $(this).removeClass("glow");
-      }
+document.addEventListener("DOMContentLoaded", function() {
+    var textbox = document.getElementById("textbox");
+    var submitButton = document.getElementById("submitButton");
+
+    // ボタンがクリックされたときの処理
+    submitButton.addEventListener("click", function() {
+        submitText();
     });
-  }
-  
-  // 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function () {
-    GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
-  
-  // 画面が読み込まれたらすぐに動かしたい場合の記述
-  $(window).on('load', function () {
-    //spanタグを追加する
-      var element = $(".glowAnime");
-    element.each(function () {
-      var text = $(this).text();
-      var textbox = "";
-      text.split('').forEach(function (t, i) {
-        if (t !== " ") {
-          if (i < 10) {
-            textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
-          } else {
-          var n = i / 10;
-            textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
-          }
-  
-        } else {
-          textbox += t;
+
+    // テキストボックスでエンターキーが押されたときの処理
+    textbox.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            submitText();
         }
-      });
-      $(this).html(textbox);
     });
-  
-    GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+    // テキストの送信処理
+    // テキストの送信処理
+function submitText() {
+    var inputText = textbox.value.trim(); // 入力されたテキストを取得
+
+    // ここでページの遷移を行うか、他の処理を行う
+    if (inputText === "あいうえお") {
+        // あいうえおが入力された場合の処理
+            // ページの遷移を行う
+        window.location.href = "page1.html"; // 新しいページのURL
+    } else {
+        // その他の場合の処理
+        alert("間違えんな");
+    }
+}
+
+});
