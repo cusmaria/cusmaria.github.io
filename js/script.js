52b0ebdc1172f0bb64bd759a1abc04b2a8038ffa
cusmaria <163232769+cusmaria@users.noreply.github.com>
2024-05-14 12:00:19 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var textbox = document.getElementById("textbox");
-    var submitButton = document.getElementById("submitButton");
+// document.addEventListener("DOMContentLoaded", function() {
+//     var textbox = document.getElementById("textbox");
+//     var submitButton = document.getElementById("submitButton");
 
-    // ボタンがクリックされたときの処理
-    submitButton.addEventListener("click", function() {
-        submitText();
-    });
+//     // ボタンがクリックされたときの処理
+//     submitButton.addEventListener("click", function() {
+//         submitText();
+//     });
 
-    // テキストボックスでエンターキーが押されたときの処理
-    textbox.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            submitText();
-        }
-    });
+//     // テキストボックスでエンターキーが押されたときの処理
+//     textbox.addEventListener("keypress", function(event) {
+//         if (event.key === "Enter") {
+//             submitText();
+//         }
+//     });
 
-    // テキストの送信処理
-    // テキストの送信処理
-function submitText() {
-    var inputText = textbox.value.trim(); // 入力されたテキストを取得
+//     // テキストの送信処理
+//     // テキストの送信処理
+// function submitText() {
+//     var inputText = textbox.value.trim(); // 入力されたテキストを取得
 
-    // ここでページの遷移を行うか、他の処理を行う
-    if (inputText === "あいうえお") {
-        // あいうえおが入力された場合の処理
-            // ページの遷移を行う
-        window.location.href = "page1.html"; // 新しいページのURL
-    } else {
-        // その他の場合の処理
-        alert("間違えんな");
-    }
-}
+//     // ここでページの遷移を行うか、他の処理を行う
+//     if (inputText === "あいうえお") {
+//         // あいうえおが入力された場合の処理
+//             // ページの遷移を行う
+//         window.location.href = "page1.html"; // 新しいページのURL
+//     } else {
+//         // その他の場合の処理
+//         alert("残念");
+//     }
+// }
 
-});
+// });
